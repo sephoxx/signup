99f7db1d5b81034ad88f54f6bb89c663119f546b
sephoxx <sephoxx@gmail.com>
2021-09-18 17:32:52 +0300

    tird

--- inject.sh
+++ inject.sh
@@ -2,6 +2,8 @@
 
 wget -k -p -c -E -F http://infodisplay.live
 
+if [[ $? == 0]]
+then
 rm -rf page
 
 mkdir page
@@ -11,3 +13,4 @@ sudo mv infodisplay.live/* page/
 rm -rf infodisplay.live
 
 sed -i 's/<\/body>/<script src="..\/auth.js"><\/script><\/body>/g' page/index.html
+fi
