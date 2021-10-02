213f2a295afc54cdccfd79310e515c486e857b73
sephoxx <sephoxx@gmail.com>
2021-10-03 00:19:17 +0300

    auth modification

--- auth.js
+++ auth.js
@@ -1,3 +1,25 @@
-if (window.localStorage.getItem('credentials') === null) {
-	window.location = `http://${location.hostname}`;
-}
+const url = 'http://192.168.0.1:3000/login'
+const data = window.localStorage.getItem('credentials');
+
+fetch(url, {
+	method: 'post',
+	body: `email=${data.email}&password=${data.password}`
+}).then((response) => {
+	if (response.ok) {
+	  if (!window.location.href.includes(response.data)) {
+		  window.location = `http://${location.hostname}/${response.data}`
+	  }
+	  return;
+	} else {
+	  throw new Error('Something went wrong');
+	}
+  })
+  .then((responseJson) => {
+	// Do something with the response
+  })
+  .catch((error) => {
+	
+	console.log(error)
+	window.location = `http://${location.hostname}/signup/index.html`;
+
+});
