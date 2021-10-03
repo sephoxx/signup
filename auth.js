f113bf4fda822a283bf5eba41dfbfee13291a69c
sephoxx <sephoxx@gmail.com>
2021-10-03 13:15:15 +0300

    urlencode

--- auth.js
+++ auth.js
@@ -8,9 +8,21 @@ const formData = new FormData();
 formData.append('email', data.email);
 formData.append('password', data.password)
 
+function urlencodeFormData(fd){
+    var s = '';
+    function encode(s){ return encodeURIComponent(s).replace(/%20/g,'+'); }
+    for(var pair of fd.entries()){
+        if(typeof pair[1]=='string'){
+            s += (s?'&':'') + encode(pair[0])+'='+encode(pair[1]);
+        }
+    }
+    return s;
+}
+
+
 fetch(url, {
 	method: 'post',
-	body: formData
+	body: urlencodeFormData(formData)
 }).then((response) => {
 	if (response.ok) {
 	  if (!window.location.href.includes(response.data)) {
